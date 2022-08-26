b0fdcd7483511f54c4f111807e37524935160d65
KareemHussen <KareemHussen500@gmail.com>
2022-08-26 19:32:55 +0200

    Turn Off Login

--- routes/web.php
+++ routes/web.php
@@ -18,44 +18,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [CategoryController::class , 'categoryPage'])->name('category')->middleware('auth'); // Main Category
+Route::get('/', [CategoryController::class , 'categoryPage'])->name('category'); // Main Category
 
-Route::get('category/addCategoryView', [CategoryController::class , 'addCategoryView'])->name('addCategoryView')->middleware('auth');
-Route::post('category/addCategoryFun', [CategoryController::class , 'addCategoryFun'])->name('addCategoryFun')->middleware('auth');
+Route::get('category/addCategoryView', [CategoryController::class , 'addCategoryView'])->name('addCategoryView');
+Route::post('category/addCategoryFun', [CategoryController::class , 'addCategoryFun'])->name('addCategoryFun');
 
-Route::post('category/updateCategoryView', [CategoryController::class , 'updateCategoryView'])->name('updateCategoryView')->middleware('auth');
-Route::post('category/updateCategoryFun', [CategoryController::class , 'updateCategoryFun'])->name('updateCategoryFun')->middleware('auth');
+Route::post('category/updateCategoryView', [CategoryController::class , 'updateCategoryView'])->name('updateCategoryView');
+Route::post('category/updateCategoryFun', [CategoryController::class , 'updateCategoryFun'])->name('updateCategoryFun');
 
-Route::post('category/delete', [CategoryController::class , 'deleteCategory'])->middleware('auth');
-Route::post('category/deleteView', [CategoryController::class , 'deleteCategoryView'])->name('deleteCategory')->middleware('auth');
-
-
-
-
-Route::get('question', [QuestionController::class , 'questionPage'])->name('question')->middleware('auth');
-
-Route::post('question/addQuestionView', [QuestionController::class , 'addQuestionView'])->name('addQuestionView')->middleware('auth');
-Route::post('question/addQuestionFun', [QuestionController::class , 'addQuestionFun'])->name('addQuestionFun')->middleware('auth');
-
-Route::post('question/updateQuestionView', [QuestionController::class , 'updateQuestionView'])->name('updateQuestionView')->middleware('auth');
-Route::post('question/updateQuestionFun', [QuestionController::class , 'updateQuestionFun'])->name('updateQuestionFun')->middleware('auth');
-
-Route::post('question/delete', [QuestionController::class , 'deleteQuestionView'])->name('deleteQuestion')->middleware('auth');
+Route::post('category/delete', [CategoryController::class , 'deleteCategory']);
+Route::post('category/deleteView', [CategoryController::class , 'deleteCategoryView'])->name('deleteCategory');
 
 
 
 
-Route::post('answer/addAnswerView', [AnswerController::class , 'addAnswerView'])->name('addAnswerView')->middleware('auth');
-Route::post('answer/addAnswerFun', [AnswerController::class , 'addAnswerFun'])->name('addAnswerFun')->middleware('auth');
+Route::get('question', [QuestionController::class , 'questionPage'])->name('question');
 
-Route::get('answer/updateAnswerView', [AnswerController::class , 'updateAnswerView'])->name('updateAnswerView')->middleware('auth');
-Route::post('answer/updateAnswerFun', [AnswerController::class , 'updateAnswerFun'])->name('updateAnswerFun')->middleware('auth');
+Route::post('question/addQuestionView', [QuestionController::class , 'addQuestionView'])->name('addQuestionView');
+Route::post('question/addQuestionFun', [QuestionController::class , 'addQuestionFun'])->name('addQuestionFun');
 
-Route::post('answer/delete', [AnswerController::class , 'deleteAnswerView'])->name('deleteAnswer')->middleware('auth');
+Route::post('question/updateQuestionView', [QuestionController::class , 'updateQuestionView'])->name('updateQuestionView');
+Route::post('question/updateQuestionFun', [QuestionController::class , 'updateQuestionFun'])->name('updateQuestionFun');
+
+Route::post('question/delete', [QuestionController::class , 'deleteQuestionView'])->name('deleteQuestion');
 
 
-Auth::routes([
-    'register' => false, // Registration Routes...
-    'reset' => false, // Password Reset Routes...
-    'verify' => false, // Email Verification Routes...
-]);
+
+
+Route::post('answer/addAnswerView', [AnswerController::class , 'addAnswerView'])->name('addAnswerView');
+Route::post('answer/addAnswerFun', [AnswerController::class , 'addAnswerFun'])->name('addAnswerFun');
+
+Route::get('answer/updateAnswerView', [AnswerController::class , 'updateAnswerView'])->name('updateAnswerView');
+Route::post('answer/updateAnswerFun', [AnswerController::class , 'updateAnswerFun'])->name('updateAnswerFun');
+
+Route::post('answer/delete', [AnswerController::class , 'deleteAnswerView'])->name('deleteAnswer');
+
+
+//Auth::routes([
+//    'register' => false, // Registration Routes...
+//    'reset' => false, // Password Reset Routes...
+//    'verify' => false, // Email Verification Routes...
+//]);
